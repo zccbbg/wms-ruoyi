b0acc1c050ec4151c5c1391189e71465073791d7
zccbbg <zccbbg@qq.com>
2024-06-14 16:40:37 +0800

    升级fastjson

--- ruoyi-common/src/main/java/com/ruoyi/common/constant/Constants.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/Constants.java
@@ -150,4 +150,9 @@ public class Constants
     public static final String LOOKUP_LDAP = "ldap://";
 
     public static final String SPAN_ID = "spanId";
+
+    /**
+     * 自动识别json对象白名单配置（仅允许解析的包名，范围越小越安全）
+     */
+    public static final String[] JSON_WHITELIST_STR = { "org.springframework", "com.ruoyi","com.cyl" };
 }
